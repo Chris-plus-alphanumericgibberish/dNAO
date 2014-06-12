4daa5734e9e95e5a2f5e896fe1c8281e6054c461
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2014-06-11 22:50:45 -0400

    Fix helms of opposite alignment so they don't crash game with binders

--- src/do_wear.c
+++ src/do_wear.c
@@ -350,7 +350,9 @@ Helmet_on()
 	case HELM_OF_OPPOSITE_ALIGNMENT:
 		if (u.ualign.type == A_NEUTRAL)
 		    u.ualign.type = rn2(2) ? A_CHAOTIC : A_LAWFUL;
-		else u.ualign.type = -(u.ualign.type);
+		else if(u.ualign.type == A_VOID){
+		    u.ualign.type = rn2(2) ? A_CHAOTIC : A_LAWFUL;
+		}else u.ualign.type = -(u.ualign.type);
 		u.ublessed = 0; /* lose your god's protection */
 	     /* makeknown(uarmh->otyp);   -- moved below, after xname() */
 		/*FALLTHRU*/
