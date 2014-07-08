e28b6d5819dd0246f9f86cf6ba6e67b1fc41525e
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2014-07-08 02:18:03 -0400

    More data

--- src/steed.c
+++ src/steed.c
@@ -668,9 +668,7 @@ int x, y;
     }
     mon->mx = x, mon->my = y;
     level.monsters[x][y] = mon;
-	pline("%d",(int)HSee_invisible);
-	pline("%d",(int)ESee_invisible);
-	pline("%d",(int)perceives(youmonst.data));
+	pline("%d",(int)monsndx(youmonst.data));
 	if (opaque(mon->data) && (!mon->minvis || See_invisible))
 		block_point(x,y);
 }
