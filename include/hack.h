52287914713910231fb433041471974270cb5e50
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-01-23 03:08:11 -0500

    hack.h 3.8
    Tabbing fix
    
    Sensorium changes:
    
    Vampires can see anything that has blood in their line of sight, regardless of invis or blindness.
    
    Metroids can see anything that is alive in their line of sight, important as they are otherwise blind.
    
    Sharks can see anything in water that is injured.

--- include/hack.h
+++ include/hack.h
@@ -124,6 +124,10 @@ NEARDATA extern coord bhitpos;	/* place where throw or zap hits or stops */
 					(u.sealsActive&SEAL_PAIMON && is_magical((mon)->data)) || \
 					(u.sealsActive&SEAL_ANDROMALIUS && is_thief((mon)->data)) || \
 					(u.sealsActive&SEAL_TENEBROUS && !nonliving((mon)->data)) || \
+					(Race_if(PM_VAMPIRE) && has_blood((mon)->data) && couldsee((mon)->mx, (mon)->my)) || \
+					(Upolyd && is_metroid(youmonst.data) && !nonliving((mon)->data) && couldsee((mon)->mx, (mon)->my)) || \
+					(Upolyd && youmonst.data == &mons[PM_SHARK] && has_blood((mon)->data) && \
+						(mon)->mhp < (mon)->mhpmax && is_pool(u.ux, u.uy) && is_pool((mon)->mx, (mon)->my)) || \
 					(u.specialSealsActive&SEAL_ACERERAK && is_undead((mon)->data)) || \
 					(uwep && uwep->oclass == WEAPON_CLASS && objects[(uwep)->otyp].oc_material == WOOD && \
 					(uwep->ovar1 & WARD_THJOFASTAFUR) && (mon->data->mlet == S_LEPRECHAUN || mon->data->mlet == S_NYMPH)) \
