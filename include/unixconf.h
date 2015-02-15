40f36a5761884a6182f2dcf85b63bc9972ebb12d
Edoardo Spadolini <kerio00@gmail.com>
2015-02-15 11:48:49 +0100

    Whoops, we previously disabled the VARDIR stuff in Makefile but not unixconf.h

--- include/unixconf.h
+++ include/unixconf.h
@@ -102,7 +102,7 @@
  * If you want the static parts of your playground on a read-only file
  * system, define VAR_PLAYGROUND to be where the variable parts are kept.
  */
-#define VAR_PLAYGROUND "/playground"
+/* #define VAR_PLAYGROUND "/playground" */
 
 
 /*
