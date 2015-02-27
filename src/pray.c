f6e830eb32f1149015bd57c3be9787036731629e
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-02-27 11:46:54 -0500

    Khor's monk ascension

--- src/pray.c
+++ src/pray.c
@@ -2609,6 +2609,7 @@ const char * const hallu_gods[] = {
 	"Khor the noble",
 	"Khor the priest",
 	"Khor the ranger",
+	"Khor the monk",
 	"ChrisANG the fourth demigod",
 	"ChrisANG the binder",
 	"FIQ the fifth demigod",
