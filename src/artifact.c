17954bb19641ebcd81859a6911029e82055af4b9
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-03-14 01:12:46 -0400

    Lost names bugfix: Special Spirits shouldn't show up on the list

--- src/artifact.c
+++ src/artifact.c
@@ -4053,6 +4053,7 @@ arti_invoke(obj)
 				long yourseals = u.sealsKnown;
 				long yourspecial = u.specialSealsKnown;
 				u.sealsKnown = obj->ovar1;
+				u.specialSealsKnown = 0;
 				u.uconduct.literate++;
 				lostname = pick_seal();
 				if(!lostname){
