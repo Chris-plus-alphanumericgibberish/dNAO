8c832e69c5d0d6f7c4094f19b68828ce5a3a1b24
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-04-24 18:07:12 -0400

    Finish Lamashtu's special attacks and resistances

--- src/sounds.c
+++ src/sounds.c
@@ -841,7 +841,15 @@ asGuardian:
 	case MS_SHRIEK:
 	    pline_msg = "shrieks.";
 	    aggravate();
-	    break;
+		if(mtmp->data == &mons[PM_LAMASHTU]){
+			struct monst *tmpm;
+			for(tmpm = fmon; tmpm; tmpm = tmpm->nmon){
+				if(tmpm->mtame) tmpm->mtame -= 10;
+				if(tmpm->mtame <= 0) tmpm->mtame = 0;
+				else tmpm->mflee = 1;
+			}
+		}
+	break;
 	case MS_IMITATE:
 	    pline_msg = "imitates you.";
 	    break;
