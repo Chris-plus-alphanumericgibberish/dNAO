f7bb1dc94ded3217ad55e532208a8e7f561bdca4
NeroOneTrueKing <azure@azenweb.org>
2017-11-02 20:15:50 -0500

    Update zap.c
    Gold scrolls of law cannot be polymorphed into scrolls (they turn into relatively useless gold pieces).

--- src/zap.c
+++ src/zap.c
@@ -1487,6 +1487,15 @@ poly_obj(obj, id)
 				(can_merge && otmp->quan > (long)rn2(1000))))
 	    otmp->quan = 1L;
 
+	if (id == STRANGE_OBJECT && obj->otyp == SCR_GOLD_SCROLL_OF_LAW)
+	{
+		/* turn gold scrolls of law into a handful of gold pieces */
+		otmp->otyp = GOLD_PIECE;
+		otmp->oclass = COIN_CLASS;
+		otmp->obj_material = GOLD;
+		otmp->quan = rnd(50 * obj->quan) + 50 * obj->quan;
+	}
+
 	switch (otmp->oclass) {
 
 	case TOOL_CLASS:
