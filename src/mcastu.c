9f7d78d3bfce1a0d7e8237cfd86c7d69c72acd50
ChrisANG <ChrisANG@atdotcom.com>
2018-06-04 14:13:39 -0400

    Spell list for half-stone dragons (groundwork for male HD noble)

--- src/mcastu.c
+++ src/mcastu.c
@@ -568,6 +568,24 @@ unsigned int type;
 				return CURE_SELF;
 		}
 	break;
+	case PM_HALF_STONE_DRAGON:
+		switch (rn2(mtmp->m_lev)) {
+			default:/* 10 -> 29*/
+				return LIGHTNING;
+			case 9:
+			case 8:
+			case 7:
+			case 6:
+			case 5:
+				return MASS_CURE_CLOSE;
+			case 4:
+			case 3:
+			case 2:
+			case 1:
+			case 0:
+				return CURE_SELF;
+		}
+	break;
 	case PM_ELF_LADY:
 	case PM_ELVENQUEEN:
 		switch (rnd(6)) {
