68a26f1635b113eb01c1234dda5749a412a7f22a
ChrisANG <ChrisANG@atdotcom.com>
2018-06-06 12:16:27 -0400

    Make fossil dark wishable

--- src/objnam.c
+++ src/objnam.c
@@ -2553,6 +2553,7 @@ struct alt_spellings {
 	{ "armored black dress", NOBLE_S_DRESS },
 	{ "droven dress", NOBLE_S_DRESS },
 	{ "armored boots", ARMORED_BOOTS },
+	{ "fossil dark", CHUNK_OF_FOSSIL_DARK },
 	{ (const char *)0, 0 },
 };
 
