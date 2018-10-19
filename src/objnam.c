b965d6d4a5cebac13270a8326c7acce4274a0334
ChrisANG <ChrisANG@atdotcom.com>
2018-10-19 13:14:29 -0400

    Add fossils and fossil monsters
    After level 14, fossils are generated like gems and gold in the rock around the dungeon.
    
    Saber-toothed cat: bite damage == two sabers (2d8)
    
    Tyrannosaurus: High level, high damage bite
    
    Triceratops: Three gore attacks
    
    Diplodocus: High level, high damage "claw" (tail slap)
    
    Trilobite: Common low level bug.
    
    Titanothere, baluchitherium, mastodon, and centipede: also found as fossils
    
    Turn Undead revives fossils as Skeleton monsters.  Stone to Flesh converts fossils to corpses (which can then be turn-undeaded into living monsters)

--- src/objnam.c
+++ src/objnam.c
@@ -834,6 +834,10 @@ boolean ignore_oquan;
 			    (index(vowels,*(mons[obj->corpsenm].mname)) ?
 								"an " : "a "),
 			mons[obj->corpsenm].mname);
+		else if (typ == FOSSIL)
+		    Sprintf(eos(buf), "%s %s",
+			mons[obj->corpsenm].mname,
+			actualn);
 		else Strcat(buf, actualn);
 		break;
 	    case BALL_CLASS:
@@ -3920,6 +3924,8 @@ typfnd:
 			if (Has_contents(otmp) && verysmall(&mons[mntmp]))
 			    delete_contents(otmp);	/* no spellbook */
 			otmp->spe = ishistoric ? STATUE_HISTORIC : 0;
+			break;
+		case FOSSIL: otmp->corpsenm = mntmp;
 			break;
 		case SCALE_MAIL:
 			/* Dragon mail - depends on the order of objects */
