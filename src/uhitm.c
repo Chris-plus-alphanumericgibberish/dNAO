49530d3422bd51fb9222bad786dbc0843cf026ea
ChrisANG <ChrisANG@atdotcom.com>
2019-12-22 21:55:29 -0500

    Fix | & confusion in vulnerable masks

--- src/uhitm.c
+++ src/uhitm.c
@@ -2506,7 +2506,7 @@ defaultvalue:
 			}
 		} else {
 			warnedptr = 0;
-			if(vulnerable_mask(resistmask) && !(weaponmask|EXPLOSION))
+			if(vulnerable_mask(resistmask) && !(weaponmask&EXPLOSION))
 				tmp *= 2;
 		}
 	}
