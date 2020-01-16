763d3ff44bc1c17f60883121f9fa93813fb8ed35
NeroOneTrueKing <azure@azenweb.org>
2020-01-15 19:08:37 -0600

    Use mdef's base level, not current level, for passive damage.
    Pre-xhity, base level was used for mhitu and mhitm passives, but defender level was used for uhitm passives.

--- src/xhity.c
+++ src/xhity.c
@@ -13822,7 +13822,7 @@ boolean endofchain;			/* if the passive is occuring at the end of aggressor's at
 	if (passive->damn)
 		dmg = d(passive->damn, passive->damd);
 	else if (passive->damd)
-		dmg = d(mlev(mdef) + 1, passive->damd);
+		dmg = d(pd->mlevel + 1, passive->damd);
 	else
 		dmg = 0;
 
