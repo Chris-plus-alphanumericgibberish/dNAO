860c637a2650b39049ae1fe8fb9e1bc272cc517e
ChrisANG <ChrisANG@none.com>
2020-04-25 18:22:55 -0400

    Fix the neutral murder bug.
    Neutral sometimes-hostile humans count as murder victims when encountered by lawfuls and chaotics, even if they spawned hostile

--- src/makemon.c
+++ src/makemon.c
@@ -10058,8 +10058,13 @@ struct monst *mtmp;
 			mtmp->malign = -3*max(3,absmal);
 		else	/* renegade */
 			mtmp->malign = max(3,absmal);
-	} else	/* not coaligned and therefore hostile */
-		mtmp->malign = abs(mal);
+	} else{	/* not coaligned and therefore hostile */
+		//Neutral murder bugfix: neutral murderables should be worth 1 algnment if hostile.
+		if(!mal && is_human(mtmp->data) && !is_derived_undead_mon(mtmp) && !mtmp->mpeaceful)
+			mtmp->malign = 1;
+		else
+			mtmp->malign = abs(mal);
+	}
 }
 
 #endif /* OVL1 */
