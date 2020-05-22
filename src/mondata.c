24345239510e0a6f52cde51e299ce16f8b425c23
NeroOneTrueKing <azure@azenweb.org>
2020-05-21 19:01:25 -0500

    Fix gnomish wizard zombies

--- src/mondata.c
+++ src/mondata.c
@@ -360,6 +360,17 @@ int faction;
 #define insert_okay (!special && (is_null_attk(attk) || (attk->aatyp > AT_HUGS && !weapon_aatyp(attk->aatyp))) && (insert = TRUE))
 #define end_insert_okay (!special && is_null_attk(attk) && (insert = TRUE))
 #define maybe_insert() if(insert) {for(j=NATTK-i-1;j>0;j--)attk[j]=attk[j-1];*attk=noattack;}
+		/* zombies/skeletons get a melee attack if they don't have any (likely due to disallowed aatyp) */
+		if ((faction == ZOMBIFIED || faction == SKELIFIED) && (
+			is_null_attk(attk) && i == 0
+			))
+		{
+			attk->aatyp = nolimbs(ptr) ? AT_BITE : AT_CLAW;
+			attk->adtyp = AD_PHYS;
+			attk->damn = ptr->mlevel / 10 + (faction == ZOMBIFIED ? 1 : 2);
+			attk->damd = max(ptr->msize * 2, 4);
+		}
+
 		/* skeletons get a paralyzing touch */
 		if (faction == SKELIFIED && (
 			insert_okay
