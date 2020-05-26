6bf197532ad26fe5fc9099f08991047e46ec51f0
NeroOneTrueKing <azure@azenweb.org>
2020-05-25 23:52:00 -0500

    Note: dragon breaths go at the front of their attack chain.

--- src/mondata.c
+++ src/mondata.c
@@ -359,7 +359,8 @@ int faction;
 
 		/* some factions want to adjust existing attacks, or add additional attacks */
 #define insert_okay (!special && (is_null_attk(attk) || \
-					((attk->aatyp > AT_HUGS && !weapon_aatyp(attk->aatyp)) || attk->aatyp == AT_NONE)) \
+					((attk->aatyp > AT_HUGS && !weapon_aatyp(attk->aatyp) \
+						&& !(attk->aatyp == AT_BREA && ptr->mlet == S_DRAGON)) || attk->aatyp == AT_NONE)) \
 					&& (insert = TRUE))
 #define end_insert_okay (!special && (is_null_attk(attk) || attk->aatyp == AT_NONE) && (insert = TRUE))
 #define maybe_insert() if(insert) {for(j=NATTK-i-1;j>0;j--)attk[j]=attk[j-1];*attk=noattack;}
