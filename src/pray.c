0e48e37ff6e9d6b0f43cf80e8cd9a3d497f6990a
ChrisANG <ChrisANG@none.com>
2020-07-13 20:13:18 -0400

    Mistweavers should also be pasified with the rest of the goat faction.

--- src/pray.c
+++ src/pray.c
@@ -4440,13 +4440,13 @@ boolean yourinvent;
 		//The Black Goat is pleased
 		struct monst *mtmp;
 		for(mtmp = migrating_mons; mtmp; mtmp = mtmp->nmon){
-			if(mtmp->mux == u.uz.dnum && mtmp->muy == u.uz.dlevel && (mtmp->mtyp == PM_BLESSED || mtmp->mtyp == PM_MOUTH_OF_THE_GOAT)){
+			if(mtmp->mux == u.uz.dnum && mtmp->muy == u.uz.dlevel && (mtmp->mtyp == PM_BLESSED || mtmp->mtyp == PM_MOUTH_OF_THE_GOAT || mtmp->mfaction == MISTWEAVER)){
 				mtmp->mpeaceful = 1;
 				set_malign(mtmp);
 			}
 		}
 		for(mtmp = fmon; mtmp; mtmp = mtmp->nmon){
-			if(goat_monster(mtmp->data)){
+			if(goat_monster(mtmp->data) || mtmp->mfaction == MISTWEAVER){
 				mtmp->mpeaceful = 1;
 				set_malign(mtmp);
 				newsym(mtmp->mx, mtmp->my);
