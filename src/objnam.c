63500842563d6814acf6984b332f548ae9050d0e
NeroOneTrueKing <azure@azenweb.org>
2020-08-25 15:55:24 -0500

    Shuriken in objnam: check len>=7 before checking spot-7

--- src/objnam.c
+++ src/objnam.c
@@ -2785,11 +2785,11 @@ const char *oldstr;
 			!strcmp(spot-4, "ninja") ||
 			!strcmp(spot-4, "ronin") ||
 			!strcmp(spot-4, "shito") ||
-			!strcmp(spot-7, "shuriken") ||
 			!strcmp(spot-4, "tengu") ||
 			!strcmp(spot-4, "manes"))) ||
 	    (len >= 6 && !strcmp(spot-5, "ki-rin")) ||
-	    (len >= 7 && !strcmp(spot-6, "gunyoki")))
+	    (len >= 7 && !strcmp(spot-6, "gunyoki")) ||
+		(len >= 8 && !strcmp(spot-7, "shuriken")))
 		goto bottom;
 
 	/* man/men ("Wiped out all cavemen.") */
