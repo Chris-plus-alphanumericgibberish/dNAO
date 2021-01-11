4eb4261cd944da952ef2d1544f6d8821d3308f25
RikerW <riker.wachtler@gmail.com>
2021-01-11 13:44:41 -0800

    Lolth mivaults need to check mon exists

--- src/mkroom.c
+++ src/mkroom.c
@@ -468,9 +468,11 @@ mkmivaultlolth()
 				if (rn2(2)) add_to_container(otmp, mkobj(SCROLL_CLASS, FALSE));
 				if (rn2(2)) add_to_container(otmp, mkobj(SCROLL_CLASS, FALSE));
 			}
-			mon->mstrategy |= STRAT_WAITFORU;
-			mon->mpeaceful = FALSE;
-			set_malign(mon);
+			if (mon){
+				mon->mstrategy |= STRAT_WAITFORU;
+				mon->mpeaceful = FALSE;
+				set_malign(mon);
+			}
 		}
 	}
 }
