5a639c28996e116ffed81b21ba06c5b705100c4a
ChrisANG <ChrisANG@none.com>
2021-05-14 23:27:14 -0400

    Fix ancient minion summoning

--- src/xhityhelpers.c
+++ src/xhityhelpers.c
@@ -491,7 +491,7 @@ demonpet()
 	i = (!is_demon(youracedata) || !rn2(6)) 
 	     ? ndemon(u.ualign.type) : NON_PM;
 	pm = i != NON_PM ? &mons[i] : youracedata;
-	if(pm->mtyp == PM_ANCIENT_OF_ICE || pm->mtyp == PM_ANCIENT_OF_DEATH) {
+	if(is_ancient(pm)) {
 	    pm = rn2(4) ? &mons[PM_METAMORPHOSED_NUPPERIBO] : &mons[PM_ANCIENT_NUPPERIBO];
 	}
 	if ((dtmp = makemon(pm, u.ux, u.uy, MM_ESUM)) != 0) {
