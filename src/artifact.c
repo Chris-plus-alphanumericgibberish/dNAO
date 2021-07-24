5496c6adca4be9636d739e86cabc9b864ead485a
NeroOneTrueKing <azure@azenweb.org>
2021-07-24 13:23:05 -0500

    Add error case for vault loot make-special

--- src/artifact.c
+++ src/artifact.c
@@ -1165,7 +1165,7 @@ mk_vault_special(otmp, vn)
 struct obj *otmp;	/* existing object */
 long long int vn;
 {
-	int type;
+	int type = -1;
 #define VN_TANNIN	0
 #define VN_ANCIENT	1
 #define VN_ANGEL	2
@@ -1201,6 +1201,9 @@ long long int vn;
 		break;
 		case VN_TANNIN:
 			otmp = mk_tannin_special(otmp);
+		break;
+		default:
+			impossible("Unhandled vault number %d for making loot special.", vn);
 		break;
 	}
 	return otmp;
