8e4df1f1459faec8bc3ba11848f89164e134114e
NeroOneTrueKing <azure@azenweb.org>
2021-08-18 12:40:42 -0500

    Handle quivering blobs' passive

--- src/xhity.c
+++ src/xhity.c
@@ -15871,6 +15871,22 @@ boolean endofchain;			/* if the passive is occuring at the end of aggressor's at
 				if (newres&MM_DEF_LSVD)
 					result |= MM_AGR_STOP;	/* attacker lifesaved */
 				break;
+				
+			case AD_PHYS:
+				/* no message, just damage, not reduced by DR */
+				if (Half_phys(magr))
+					dmg = (dmg + 1) / 2;
+				if (youagr && u.uvaul_duration)
+					dmg = (dmg + 1) / 2;
+				if (dmg < 1)
+					dmg = 1;
+
+				newres = xdamagey(mdef, magr, &noattack, dmg);
+				if (newres&MM_DEF_DIED)
+					result |= MM_AGR_DIED;	/* attacker died */
+				if (newres&MM_DEF_LSVD)
+					result |= MM_AGR_STOP;	/* attacker lifesaved */
+				break;
 
 			case AD_EACD:
 			case AD_ACID:
