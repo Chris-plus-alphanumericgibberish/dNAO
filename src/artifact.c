162359c9add432b17510012c1d4d1932583cff28
ChrisANG <ChrisANG@none.com>
2021-09-12 13:13:04 -0400

    malleable artifacts may even have different base item types
    Gungnir only worked for atgeir :(

--- src/artifact.c
+++ src/artifact.c
@@ -1438,7 +1438,7 @@ register boolean mod;
 
 	if (otmp && *name)
 	    for (a = artilist+1; a->otyp; a++)
-		if (a->otyp == otmp->otyp && !strcmp(a->name, name)) {
+		if ((a->otyp == otmp->otyp || (is_malleable_artifact(a) && artitypematch(a, otmp))) && !strcmp(a->name, name)) {
 		    register int m = a - artilist;
 		    otmp->oartifact = (mod ? m : 0);
 		    otmp->age = 0;
