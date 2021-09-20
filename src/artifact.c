bf9007f92d42ab1efe8034e9341fd0df82c57ed4
NeroOneTrueKing <azure@azenweb.org>
2021-09-20 17:59:40 -0500

    Buff Mirrored Mask to be less prone to failure
    1) a monster resisting the effect doesn't set the artifact timeout on the mask (though you still waste a turn)
    2) allow invoking while Unchanging; you just won't poly.
    
    Should make it a lot less frustrating to use

--- src/artifact.c
+++ src/artifact.c
@@ -8546,10 +8546,6 @@ arti_invoke(obj)
 				/* revert */
 				rehumanize();
 			}
-			else if (Unchanging) {
-				You_feel("the mask's magic be blocked by something.");
-				return partial_action();
-			}
 			else {
 				/* steal a face */
 				if(getdir((char *)0)) {
@@ -8566,13 +8562,14 @@ arti_invoke(obj)
 							xkilled(mtmp, 3);
 							obj->corpsenm = mtmp->mtyp;
 							/* keep consistent with on-wear code in do_wear.c */
-							if (obj == ublindf) {
+							if (obj == ublindf && !Unchanging) {
 								activate_mirrored_mask(obj);
 							}
 						}
 						else {
 							/* resisted */
 							pline("%s resists!", Monnam(mtmp));
+							obj->age = monstermoves;	// but does use your turn
 						}
 					}
 				}
