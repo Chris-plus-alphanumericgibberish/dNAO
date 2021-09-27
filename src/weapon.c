26253e7ac000eccfbe8989da5065bc27a56e4914
RikerW <riker.wachtler@gmail.com>
2021-09-27 14:36:14 -0400

    Fix Rod of Force inspect skill
    If you had a shield or offhand weapon, it would not set a type before returning.

--- src/weapon.c
+++ src/weapon.c
@@ -3062,10 +3062,8 @@ struct obj *obj;
 		else if(obj->otyp == DOUBLE_LIGHTSABER && !obj->altmode){
 			CHECK_ALTERNATE_SKILL(P_TWO_HANDED_SWORD)
 		}
-		else if(obj->otyp == ROD_OF_FORCE){
-			if(!uarms && !u.twoweap){
-				CHECK_ALTERNATE_SKILL(P_TWO_HANDED_SWORD)
-			}
+		else if(obj->otyp == ROD_OF_FORCE && !uarms && !u.twoweap){
+			CHECK_ALTERNATE_SKILL(P_TWO_HANDED_SWORD)
 		}
 		else if(obj->otyp == KHOPESH){
 			CHECK_ALTERNATE_SKILL(P_AXE)
