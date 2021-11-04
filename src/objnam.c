96848fc3ce7a57640326d1b0927e13e91be01451
NeroOneTrueKing <azure@azenweb.org>
2021-11-04 13:06:23 -0500

    vtense should fix "were"->"was"

--- src/objnam.c
+++ src/objnam.c
@@ -2763,6 +2763,8 @@ register const char *verb;
 
 	if (!strcmp(verb, "are"))
 	    Strcpy(buf, "is");
+	else if (!strcmp(verb, "were"))
+	    Strcpy(buf, "was");
 	else if (!strcmp(verb, "have"))
 	    Strcpy(buf, "has");
 	else if (index("zxs", *spot) ||
