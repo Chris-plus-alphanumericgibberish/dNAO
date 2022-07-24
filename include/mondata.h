1c9c9b92c2df11de6729929555a2c597cb9da5e2
chris <ohmygod.stopitwiththis.com>
2022-07-24 11:25:10 -0400

    Minor monster revisions
    Autons are spiritual beings (MA_MINION)
    -They do not summon more autons with their attacks.
    
    Make disenchanters a bit scarier.
    -Give them a weak (tongue) touch attack.
    -Give them a 4d4 passive tail slap attack
    -Mark the claw attack as polywep
    
    Shalosh tannah can dig (since their flesh hooks can be used as digging tools).
    
    Kraken isn't supposed to be an animal. This was a copy-paste error from regular krakens.
    
    Reduce Ghoul-queen Nitocris's AC. This should make her easier to contend with if an unspoiled player powers through the Nitocris phase and doesn't know how to stop the Ghoul-queen phase from resurecting.

--- include/mondata.h
+++ include/mondata.h
@@ -551,7 +551,7 @@
 #define	is_snake_bite_mtyp(ptr)	((ptr)->mtyp == PM_MEDUSA \
 									|| (ptr)->mtyp == PM_ANCIENT_NAGA)
 #define	is_snake_bite_mon(mon)	(is_snake_bite_mtyp((mon)->data))
-#define	is_tailslap_mtyp(ptr)	(is_true_adult_dragon(ptr) || (ptr)->mtyp == PM_UISCERRE_ELADRIN)
+#define	is_tailslap_mtyp(ptr)	(is_true_adult_dragon(ptr) || (ptr)->mtyp == PM_UISCERRE_ELADRIN || (ptr)->mtyp == PM_DISENCHANTER)
 #define	is_tailslap_mon(mon)	(is_tailslap_mtyp((mon)->data))
 
 #define	is_vines_mon(mon)	((mon)->mtyp == PM_GAE_ELADRIN && (mon)->m_lev >= 20)
@@ -611,6 +611,7 @@
 							)
 
 #define gates_in_help(ptr)	((is_demon((ptr)) || is_minion((ptr))) \
+								&& !is_auton(ptr) \
 								&& (ptr)->mtyp != PM_OONA \
 								&& (ptr)->mtyp != PM_BALROG \
 								&& (ptr)->mtyp != PM_DURIN_S_BANE \
