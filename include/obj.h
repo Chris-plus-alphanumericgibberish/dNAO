892c326204106297a42471c89be86e0771dc411b
chris <ohmygod.stopitwiththis.com>
2022-08-05 21:43:42 -0400

    Singing sword can be a long sword, rapier, or saber.

--- include/obj.h
+++ include/obj.h
@@ -448,6 +448,7 @@ struct obj {
 							( (a)->otyp ) == UNIVERSAL_KEY ? ((o)->otyp==SKELETON_KEY) : \
 							( (a)->otyp ) == ROUNDSHIELD ? ((o)->otyp==DWARVISH_ROUNDSHIELD) : \
 							( (a) == &artilist[ART_GUNGNIR] ) ? (is_spear(o)) : \
+							( (a) == &artilist[ART_SINGING_SWORD] ) ? ((o)->otyp == LONG_SWORD || (o)->otyp == RAPIER || (o)->otyp == SABER) : \
 							( (a) == &artilist[ART_DIRGE] ) ? (o->oclass == WEAPON_CLASS || is_weptool(o)) : \
 							((a) == &artilist[ART_FIRE_BRAND] || (a) == &artilist[ART_FROST_BRAND]) ? \
 								(u.brand_otyp == STRANGE_OBJECT ? \
