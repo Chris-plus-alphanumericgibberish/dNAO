7d38ef143829629e35875c0b60e9c7a4e323d515
chris <ohmygod.stopitwiththis.com>
2022-08-22 17:34:40 -0400

    Veil renders can also reach around extra-dimensionally

--- src/xhity.c
+++ src/xhity.c
@@ -2773,7 +2773,7 @@ int vis;
 	}
 	
 	//Star spawn bypass most helmets.
-	if(magr && magr->mtyp == PM_STAR_SPAWN && obj && obj->oartifact != ART_APOTHEOSIS_VEIL)
+	if(magr && (magr->mtyp == PM_STAR_SPAWN || magr->mtyp == PM_VEIL_RENDER) && obj && obj->oartifact != ART_APOTHEOSIS_VEIL)
 		return FALSE;
 
 	if (obj && (
@@ -7597,7 +7597,7 @@ boolean ranged;
 
 		/* protected by helmets */
 		otmp = (youdef ? uarmh : which_armor(mdef, W_ARMH));
-		if(pa->mtyp == PM_STAR_SPAWN && otmp && is_hard(otmp) && otmp->oartifact != ART_APOTHEOSIS_VEIL){
+		if((pa->mtyp == PM_STAR_SPAWN || pa->mtyp == PM_VEIL_RENDER) && otmp && is_hard(otmp) && otmp->oartifact != ART_APOTHEOSIS_VEIL){
 			if (youdef) {
 				pline("%s somehow reaches right past your helmet!", Monnam(magr));
 			}
