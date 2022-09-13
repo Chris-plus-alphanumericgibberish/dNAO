d2305c74b9d8d4611b10dff446f234014a52cd97
NeroOneTrueKing <azure@azenweb.org>
2022-09-12 22:14:52 -0500

    Two more starlight fixes

--- src/mon.c
+++ src/mon.c
@@ -5353,7 +5353,7 @@ register struct monst *mdef;
 			if (flooreffects(obj, x, y, "fall")) continue;
 			place_object(obj, x, y);
 		    } else {
-			if (obj->lamplit) end_burn(obj, TRUE);
+			if (obj_is_burning(obj)) end_burn(obj, TRUE);
 			obj->nobj = oldminvent;
 			oldminvent = obj;
 		    }
@@ -5411,7 +5411,7 @@ register struct monst *mdef;
 			if (flooreffects(obj, x, y, "fall")) continue;
 			place_object(obj, x, y);
 		    } else {
-			if (obj->lamplit) end_burn(obj, TRUE);
+			if (obj_is_burning(obj)) end_burn(obj, TRUE);
 			obj->nobj = oldminvent;
 			oldminvent = obj;
 		    }
@@ -5489,7 +5489,7 @@ register struct monst *mdef;
 			if (flooreffects(obj, x, y, "fall")) continue;
 			place_object(obj, x, y);
 		    } else {
-			if (obj->lamplit) end_burn(obj, TRUE);
+			if (obj_is_burning(obj)) end_burn(obj, TRUE);
 			obj->nobj = oldminvent;
 			oldminvent = obj;
 		    }
@@ -5546,7 +5546,7 @@ register struct monst *mdef;
 			if (flooreffects(obj, x, y, "fall")) continue;
 			place_object(obj, x, y);
 		    } else {
-			if (obj->lamplit) end_burn(obj, TRUE);
+			if (obj_is_burning(obj)) end_burn(obj, TRUE);
 			obj->nobj = oldminvent;
 			oldminvent = obj;
 		    }
