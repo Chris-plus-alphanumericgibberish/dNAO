ba2a824c8b66841093cab4e7d8c3b13d95f1aa05
chris <ohmygod.stopitwiththis.com>
2023-05-03 22:46:48 -0400

    Bugfix: Adjust elf healer starting align to neutral

--- src/u_init.c
+++ src/u_init.c
@@ -2497,6 +2497,11 @@ u_init()
 			HTelepat |= FROMOUTSIDE;
 			skill_add(Skill_Elf_Ana);
 		}
+		if(Role_if(PM_ELF)){
+			u.ualign.type = A_NEUTRAL;
+			u.ualign.god = u.ugodbase[UGOD_CURRENT] = u.ugodbase[UGOD_ORIGINAL] = align_to_god(u.ualign.type);
+			flags.initalign = 1; // 1 == neutral
+		}
 	    /* Elves can recognize all elvish objects */
 		if(!Role_if(PM_MADMAN)){ /*Madmen have been amnesticized*/
 			knows_object(ELVEN_SHORT_SWORD);
