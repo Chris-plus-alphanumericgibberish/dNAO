8b73f5818e9d4a69cefef85d00cdf717fb3d635c
Ron Nazarov <ron@noisytoot.org>
2023-06-13 20:48:23 +0100

    Fix gcc warning

--- src/teleport.c
+++ src/teleport.c
@@ -75,7 +75,7 @@ unsigned gpflags;
 				return !!(Levitation || Flying || Wwalking);
 			if (mtmp == &youmonst)
 				return !!(Levitation || Flying || Wwalking ||
-					  (Swimming || Amphibious) && Waterproof && !(u.sealsActive&SEAL_OSE));
+					  (Swimming || Amphibious) && (Waterproof && !(u.sealsActive&SEAL_OSE)));
 			else	return (mon_resistance(mtmp,FLYING) || breathless_mon(mtmp) || mon_resistance(mtmp,SWIMMING) ||
 								is_clinger(mdat) || amphibious_mon(mtmp));
 	    } else if (mdat->mlet == S_EEL && !ignorewater && rn2(13)) {
