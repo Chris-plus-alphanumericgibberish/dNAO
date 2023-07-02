0ae54cb781bfd1dcc1dacd103fbb679e7473c867
chris <ohmygod.stopitwiththis.com>
2023-07-02 13:50:14 -0400

    Swap out mindless thralls for hostile trooper husks in future
    Adjust genesis handling to give them the correct faction.

--- src/read.c
+++ src/read.c
@@ -3905,7 +3905,7 @@ char *in_buff;
 createmon:
 		for (i = 0; i <= (allow_multi ? multi : 0); i++) {
 			if (monclass != MAXMCLASSES && !(ma_require || mg_restrict || gen_restrict))
-				whichpm = mkclass(monclass, G_NOHELL | G_HELL | G_PLANES);
+				whichpm = mkclass(monclass, G_NOHELL | G_HELL | G_PLANES | G_DEPTHS);
 
 			int mm_flags = NO_MM_FLAGS;
 			if (maketame)
@@ -3925,8 +3925,11 @@ createmon:
 				}
 				else if (makepeaceful)
 					mtmp->mpeaceful = 1;
-				else if (makehostile)
+				else if (makehostile){
 					mtmp->mpeaceful = 0;
+					if(Infuture)
+						set_faction(mtmp, ILSENSINE_FACTION);
+				}
 				set_malign(mtmp);
 
 				if (makesummoned)
