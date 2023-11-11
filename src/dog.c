2e63081795aff1883adc2f1c9f4752c5c8d4563a
chris <ohmygod.stopitwiththis.com>
2023-11-11 18:55:02 -0500

    Vampire spawn don't lose tameness while you're off level

--- src/dog.c
+++ src/dog.c
@@ -663,6 +663,7 @@ long nmv;		/* number of moves */
 
 	/* reduce tameness for every 150 moves you are separated */
 	if (get_mx(mtmp, MX_EDOG) && !(EDOG(mtmp)->loyal) && !(EDOG(mtmp)->dominated)
+	 && !(Race_if(PM_VAMPIRE) && is_vampire(mtmp->data))
 	 && !(
 	  In_quest(&u.uz) 
 	  && ((Is_qtown(&u.uz) && !flags.stag) || 
