7c816696a786ae26acc9128bc8ce07359d82fdc0
chris <ohmygod.stopitwiththis.com>
2024-03-31 20:39:41 -0400

    Display classic colors option in-game

--- src/options.c
+++ src/options.c
@@ -80,7 +80,7 @@ static struct Bool_Opt
 #endif
 #ifdef CURSES_GRAPHICS
 	{"classic_status", &iflags.classic_status, TRUE, SET_IN_FILE},
-	{"classic_colors", &iflags.classic_colors, TRUE, SET_IN_FILE},
+	{"classic_colors", &iflags.classic_colors, TRUE, DISP_IN_GAME},
 #endif
 	{"cmdassist", &iflags.cmdassist, TRUE, SET_IN_GAME},
 # if defined(MICRO) || defined(WIN32) || defined(CURSES_GRAPHICS)
