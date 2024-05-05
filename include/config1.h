f20de2cba6b386ee10db0fdfcbdfb1e30787f3d9
Ron Nazarov <ron@noisytoot.org>
2024-05-05 01:48:04 +0100

    Fix implicit function declaration in {lev,dgn}_comp.l
    GCC 14 (and Clang with -std=c2x) defaults to
    -Werror=implicit-function-declaration.
    
    Also add #define _POSIX_C_SOURCE 200809L to config1.h.  This doesn't
    actually fix the problem (since lex adds the stdio.h include before
    anything user-provided), but should be there anyway.
    
    # Conflicts:
    #	include/config1.h

--- include/config1.h
+++ include/config1.h
@@ -5,6 +5,8 @@
 #ifndef CONFIG1_H
 #define CONFIG1_H
 
+#define _POSIX_C_SOURCE 200809L
+
 /*
  * MS DOS - compilers
  *
@@ -128,7 +130,6 @@
 #endif
 
 #endif
-
 
 #if defined(__linux__) && defined(__GNUC__) && !defined(_GNU_SOURCE)
 /* ensure _GNU_SOURCE is defined before including any system headers */
