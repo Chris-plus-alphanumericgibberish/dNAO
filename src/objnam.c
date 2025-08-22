26d4d228eef83b45726c061dadaef6bffba501fa
zoe chen <baoliejuan@gmail.com>
2025-08-22 10:18:02 +0800

    Allow wishing for specific Sky Reflected weapon type instead of telling it's already born

--- src/objnam.c
+++ src/objnam.c
@@ -6181,6 +6181,9 @@ typfnd:
 		aname = artifact_name(name, &objtyp, NULL);
 
 		if (aname) {
+			if (!strcmp((&artilist[ART_SKY_REFLECTED])->name, aname))
+				set_material_gm(otmp, MERCURIAL);
+
 			/* attempt to create an artifact with the modified name */
 			otmp = oname(otmp, aname);
 
