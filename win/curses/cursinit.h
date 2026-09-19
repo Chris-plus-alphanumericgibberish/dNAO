f323b2cb6e070d1ae3df518ea63ddf0452220c0d
RikerW <riker.wachtler@gmail.com>
2026-09-19 01:39:38 -0700

    Fix the curses game-start code to properly reference race letters
    Now it won't just use the first character. Also sorta extends this to function properly if later, it becomes necessary to do that for roles as well (dude, it's not that far fetched)

--- win/curses/cursinit.h
+++ win/curses/cursinit.h
@@ -8,7 +8,7 @@
 void curses_create_main_windows(void);
 void curses_init_nhcolors(void);
 void curses_choose_character(void);
-int curses_character_dialog(const char **choices, const char *prompt);
+int curses_character_dialog(const char **choices, char *letters, const char *prompt);
 void curses_init_options(void);
 void curses_display_splash_window(void);
 void curses_cleanup(void);
